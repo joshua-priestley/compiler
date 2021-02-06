6162e3531a115076523b260a89e298b60da80f14
Euan Imperial <es1519@ic.ac.uk>
2021-02-06 21:33:11 +0000

    Started the symbol table for semantic analysis

---
+++ src/main/kotlin/SymbolTable.kt
@@ -0,0 +1,45 @@
+class SymbolTable(val parentT: SymbolTable?) {
+    val functions = HashMap<Ident, FunctionNode>()
+    val variables = HashMap<Ident, TypeNode>()
+    val table = HashMap<Ident, AssignRHSNode>()
+
+    fun addFunction(function: FunctionNode) {
+        if (functions.containsKey(function.ident)) {
+            println("Function already exists")
+        }
+        functions[function.ident] = function
+    }
+
+    fun addVariable(variable: DeclarationNode) {
+        if (variables.containsKey(variable.ident)) {
+            println("Variable already exists")
+        }
+        variables[variable.ident] = variable.type
+        table[variable.ident] = variable.value
+    }
+
+    fun getType(ident: Ident): TypeNode? {
+        if (variables.containsKey(ident)) {
+            return variables[ident]
+        }
+
+        if (parentT == null) {
+            return null
+        }
+
+        return parentT.getType(ident)
+    }
+
+    fun getExpr(ident: Ident): AssignRHSNode? {
+        if (table.containsKey(ident)) {
+            return table[ident]
+        }
+
+        if (parentT == null) {
+            return null
+        }
+
+        return parentT.getExpr(ident)
+    }
+
+}
