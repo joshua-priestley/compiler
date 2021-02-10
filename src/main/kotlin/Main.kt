84cb9f4858b3c49dec8e63b8e21090c1bae638b1
RobJBarr <robjb12@gmail.com>
2021-02-10 18:37:22 +0000

    Fixed return types for check() and compile()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ package compiler
 
 import ProgramNode
 import SemanticWalker
-import SymbolTable
 import Visitor
 import WACCErrorListener
 import org.antlr.v4.runtime.*
@@ -44,7 +43,7 @@ class Compiler(val inputFile: String) {
 
         if (listener.hasSyntaxErrors()) {
             listener.printSyntaxErrors()
-            return false
+            return 100
         }
 
         println(tree.toStringTree(parser))
@@ -60,19 +59,14 @@ class Compiler(val inputFile: String) {
 
         if (listener.hasSyntaxErrors()) {
             listener.printSyntaxErrors()
-            return false
+            return 100
         }
 
-        return true
+        return 0
     }
 
     fun compile(): Int {
-        val result = check()
-        return if (result) {
-            0
-        } else {
-            100
-        }
+        return check()
 
     }
 }
