cd7410872099dc510fd755c258461acddc34fbbb
RobJBarr <robjb12@gmail.com>
2021-02-10 19:42:41 +0000

    Refactored producing a message, added comments, still need to implement getting line and char of error

--- src/main/kotlin/SemanticErrorHandler.kt
+++ src/main/kotlin/SemanticErrorHandler.kt
@@ -1,43 +1,88 @@
 import kotlin.Int
+import kotlin.system.exitProcess
 
+const val ERROR_CODE = 200
+const val BASE_MSG = "Semantic Error at "
 class SemanticErrorHandler {
-    private final val ERROR_CODE = 200
     private val errorList: MutableCollection<String> = mutableListOf()
+
+    //Error message for accessing element of null pair
     fun nullTypeAccess() {
-        val errorMsg = "accessing element of null pair"
-        errorList.add(errorMsg)
+        val line = errorLine()
+        val char = errorChar()
+        val msg = "accessing element of null pair"
+        val fullMsg = buildErrorMessage(msg,line,char)
+
+        errorList.add(fullMsg)
     }
 
+    //Error message for incompatibleType
     fun incompatibleType(type: Type) {
-        val errorMsg = "Incompatible type " + type.toString()
-        errorList.add(errorMsg)
+        val line = errorLine()
+        val char = errorChar()
+        val msg = "Incompatible type $type"
+        val fullMsg = buildErrorMessage(msg,line,char)
+
+        errorList.add(fullMsg)
     }
 
+    //Error message for an undefined variable
     fun undefinedVariable(ident: String) {
-        val errorMsg = "Variable " + ident + " is not defined in this scope"
-        errorList.add(errorMsg)
+        val line = errorLine()
+        val char = errorChar()
+        val msg = "Variable $ident is not defined in this scope"
+        val fullMsg = buildErrorMessage(msg,line,char)
+
+        errorList.add(fullMsg)
     }
 
+    //Error message for incompatible types in an expression
     fun incompatibleTypes(expr: String, expected: Type, actual: Type) {
-        val errorMsg = "Incompatible type at " + expr + " (expected: " + expected + ", actual: " + actual + ")"
-        errorList.add(errorMsg)
+        val line = errorLine()
+        val char = errorChar()
+        val msg = "Incompatible type at $expr (expected: $expected, actual: $actual)"
+        val fullMsg = buildErrorMessage(msg,line,char)
+
+        errorList.add(fullMsg)
     }
 
+    //Error message for returning from global scope
     fun returnFromGlobal(){
-        val errorMsg = "Cannot return from the global scope."
-        errorList.add(errorMsg)
+        val line = errorLine()
+        val char = errorChar()
+        val msg = "Cannot return from the global scope."
+        val fullMsg = buildErrorMessage(msg,line,char)
+
+        errorList.add(fullMsg)
+    }
+    //Build a full error message given the message, line, and char of the error
+    private fun buildErrorMessage(msg : String, line : Int, char : Int) : String{
+        val sb = StringBuilder(BASE_MSG)
+        sb.append(line)
+        sb.append(':')
+        sb.append(char)
+        sb.append(" -- ")
+        sb.append(msg)
+        return sb.toString()
     }
 
-    fun errorLine() : Int{
-        return 0;
+    //Get the line of the error
+    private fun errorLine() : Int{
+        return 0
     }
 
+    //Get the character of the error
+    private fun errorChar() : Int{
+        return 0
+    }
+
+    //Iterate through the list of errors and print out the error and the line and character where it occurs
     fun printErrors() {
         val errorsIterator = errorList.iterator()
         while (errorsIterator.hasNext()){
             System.err.println(errorsIterator.next())
         }
-        System.exit(ERROR_CODE)
+        exitProcess(ERROR_CODE)
     }
 
 }
