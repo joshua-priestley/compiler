bc36d65610fc27348b2f7533de64c8fda82174f1
Euan Imperial <es1519@ic.ac.uk>
2021-02-20 19:23:44 +0000

    Load instruction v1 done

--- src/main/kotlin/Instructions/Load.kt
+++ src/main/kotlin/Instructions/Load.kt
@@ -1,9 +1,40 @@
 package compiler.Instructions
 
-class Load : Instruction {
+import java.lang.StringBuilder
 
+class Load : Instruction {
+    private val dstReg: Register
+    private val srcReg: Register?
+    private val srcValue: String?
+    private val offset: Int
+
+    constructor(dstReg: Register, srcValue: String) {
+        this.dstReg = dstReg
+        this.srcValue = srcValue
+        this.srcReg = null
+        this.offset = 0
+    }
+
+    constructor(dstReg: Register, srcReg: Register) {
+        this.dstReg = dstReg
+        this.srcReg = srcReg
+        this.srcValue = null
+        this.offset = 0
+    }
 
     override fun toString(): String {
-        TODO("Not yet implemented")
+        val instr = StringBuilder()
+        instr.append("\tLDR")
+        instr.append(" $dstReg, ")
+        if (srcReg != null) {
+            instr.append("[$srcReg")
+            if (offset != 0) {
+                instr.append(" , #$offset")
+            }
+            instr.append("]")
+        } else {
+            instr.append("=$srcValue")
+        }
+        return instr.toString()
     }
 }
