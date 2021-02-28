28fad50fd2de4c690945ff6a0064df53ea795964
Euan Imperial <es1519@ic.ac.uk>
2021-02-28 19:23:50 +0000

    Created a parent class to reduce code duplication with addition and subtraction.

--- src/main/kotlin/Instructions/Sub.kt
+++ src/main/kotlin/Instructions/Sub.kt
@@ -1,35 +1,8 @@
 package compiler.Instructions
 
-class Sub {
-    private val dstReg : Register
-    private val srcReg1 : Register?
-    private val srcReg2 : Register?
-    private val s : Boolean
-    private val imm : Int?
+class Sub: AddSub {
 
-    constructor(dstReg : Register, srcReg1 : Register, srcReg2: Register, s : Boolean){
-        this.dstReg = dstReg
-        this.srcReg1 = srcReg1
-        this.srcReg2 = srcReg2
-        this.s = s
-        this.imm = null
-    }
+    constructor(dstReg : Register, srcReg1 : Register, srcReg2: Register, s : Boolean) : super("SUB", dstReg, srcReg1, srcReg2, s)
 
-    constructor(dstReg: Register, srcReg1: Register, imm : Int, s : Boolean){
-        this.dstReg = dstReg
-        this.srcReg1 = srcReg1
-        this.srcReg2 = null
-        this.s = s
-        this.imm = imm
-    }
-    override fun toString(): String {
-        val sb : StringBuilder = StringBuilder("SUB")
-        sb.append(if(s) "S " else " ")
-        sb.append("$dstReg, ")
-        if (srcReg1 != null) sb.append("$srcReg1, ")
-        if (srcReg2 != null) sb.append("$srcReg2, ")
-        if (imm != null) sb.append("#$imm")
-
-        return sb.toString()
-    }
+    constructor(dstReg: Register, srcReg1: Register, imm : Int, s : Boolean) : super("SUB", dstReg, srcReg1, imm, s)
 }
