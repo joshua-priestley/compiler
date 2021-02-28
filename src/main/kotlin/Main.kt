ca043c921d8655c2223d1c9b5330bee9752e6bc4
Joshua Priestley <joshuapriestley@Joshuas-MacBook-Pro.local>
2021-02-28 15:51:09 +0000

    Made the Main file write the files out to run a LabTS test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -75,9 +75,9 @@ class Compiler(private val inputFile: String, private val assembly: Boolean = fa
             val listOfInstructions = codeGeneration.generateProgram(root as ProgramNode)
             val instructions = listOfInstructions.joinToString(separator = "\n") + "\n"
 
-            val assemblyFileName = inputFile.replace(".wacc", ".s")
+            val assemblyFileName = file.name.replace(".wacc", ".s")
             println(instructions)
-            //File(assemblyFileName).writeText(instructions)
+            File(assemblyFileName).writeText(instructions)
         }
 
         println("Successfully finished compilation with exit code 0.")
