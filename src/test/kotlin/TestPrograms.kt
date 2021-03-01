a1c83bbb8d801b28c5c06d0586399530b19b7b64
Joshua Priestley <joshuapriestley@Joshuas-MacBook-Pro.local>
2021-03-01 19:03:02 +0000

    Adjusted testing so all tests are now done in TestPrograms.kt

--- src/test/kotlin/TestPrograms.kt
+++ src/test/kotlin/TestPrograms.kt
@@ -1,13 +1,22 @@
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FileDataPart
+import com.github.kittinunf.fuel.core.InlineDataPart
+import com.github.kittinunf.fuel.gson.responseObject
+import com.google.gson.Gson
 import compiler.Compiler
 import org.junit.jupiter.api.Assertions.assertEquals
 
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.DynamicTest
 import java.io.File
+import java.lang.StringBuilder
+
+data class CompilerReply(val test: String, val upload: String, val compiler_out: String)
 
 class TestPrograms {
     private val testDirsPath = "./src/test/kotlin/testDirs"
     private val examplesPath = "./wacc_examples/"
+    private val gson by lazy { Gson() }
 
     // All directories specified in the testDirs file
     private val allTestDirs = File(testDirsPath).readLines()
@@ -28,6 +37,35 @@ class TestPrograms {
             inputFile.canonicalPath.contains("semantic") -> assertEquals(200, ret)
             inputFile.canonicalPath.contains("valid") -> assertEquals(0, ret)
         }
+
+        if(System.getProperty("test.type") == "backend") {
+            val assemblyName = inputFile.absolutePath.replace(".wacc", ".s");
+            val executableName = inputFile.absolutePath.replace(".wacc", "");
+
+            Runtime.getRuntime()
+                .exec("arm-linux-gnueabi-gcc -o $executableName -mcpu=arm1176jzf-s -mtune=arm1176jzf-s $assemblyName")
+
+            val process = ProcessBuilder("qemu-arm", "-L", "/usr/arm-linux-gnueabi/", executableName).start()
+
+            val sb = StringBuilder()
+            process.inputStream.reader(Charsets.UTF_8).use {
+                sb.append(it.readText())
+            }
+
+            val a = Fuel.upload("https://teaching.doc.ic.ac.uk/wacc_compiler/run.cgi")
+                .add(FileDataPart(inputFile, "testfile", inputFile.name, "application/octet-stream"))
+                .add(InlineDataPart("-x","options[]"))
+                .responseObject<CompilerReply>(gson).third
+                .component1()
+
+            println(sb.toString())
+
+            if (a != null) {
+                val strs = a.compiler_out.split("===========================================================").toTypedArray()
+                println(strs[1])
+                assertEquals(strs[1], "\n" + sb)
+            }
+        }
     }
 
     @TestFactory
@@ -36,6 +74,7 @@ class TestPrograms {
             "syntax" -> testFiles.filter { it.absolutePath.contains("/syntaxErr/") }
             "semantic" -> testFiles.filter { it.absolutePath.contains("/semanticErr/") }
             "valid" -> testFiles.filter { it.absolutePath.contains("/valid/") }
+            "backend" -> testFiles.filter { it.absolutePath.contains("/valid/") }
             else -> testFiles
         }
         return files.map { f -> DynamicTest.dynamicTest(f.name) { runTest(f) } }
