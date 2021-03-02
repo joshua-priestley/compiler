8cba13c0788186fc26370138bd390eec5702f31a
Euan Imperial <es1519@ic.ac.uk>
2021-03-02 12:43:48 +0000

    Added a boolean to the store instruction for if its a parameter.

--- src/main/kotlin/Instructions/Store.kt
+++ src/main/kotlin/Instructions/Store.kt
@@ -5,19 +5,22 @@ class Store : Instruction {
     private val dstAddress: Register
     private val offset: Int
     private val cond: Conditions?
+    private val parameter: Boolean
 
-    constructor(srcReg: Register, dstAddress: Register, offset: Int, condition: Conditions? = null) {
+    constructor(srcReg: Register, dstAddress: Register, offset: Int, condition: Conditions? = null, parameter: Boolean = false) {
         this.srcReg = srcReg
         this.dstAddress = dstAddress
         this.offset = offset
         this.cond = condition
+        this.parameter = parameter
     }
 
-    constructor(srcReg: Register, dstAddress: Register, condition: Conditions? = null) {
+    constructor(srcReg: Register, dstAddress: Register, condition: Conditions? = null, parameter: Boolean = false) {
         this.srcReg = srcReg
         this.dstAddress = dstAddress
         this.offset = 0
         this.cond = condition
+        this.parameter = parameter
     }
 
     override fun toString(): String {
@@ -27,6 +30,7 @@ class Store : Instruction {
         output.append(" $srcReg, [$dstAddress")
         if (offset != 0) output.append(", #$offset")
         output.append("]")
+        if (parameter) output.append("!")
         return output.toString()
     }
 }
