681f279727975427805497bd922e0cc81ff5cec1
Euan Imperial <es1519@ic.ac.uk>
2021-03-02 15:04:48 +0000

    Changed nextAvailable() function in Register.kt

--- src/main/kotlin/Instructions/Register.kt
+++ src/main/kotlin/Instructions/Register.kt
@@ -1,9 +1,11 @@
 package compiler.Instructions
 
+const val NO_GEN_PURPOSE_REG = 13
+
 enum class Register {
     R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, SP, LR, PC, CPSR;
 
     fun nextAvailable(): Register {
-        return values()[(this.ordinal + 1) % values().size]
+        return values()[(this.ordinal + 1) % NO_GEN_PURPOSE_REG]
     }
 }
