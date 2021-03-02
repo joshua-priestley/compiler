dba0ce8aa1f989272ca910b7cc3de34c696d09dd
RobJBarr <robjb12@gmail.com>
2021-03-02 22:30:45 +0000

    Added tab character to AndOr toString() method

--- src/main/kotlin/Instructions/AndOr.kt
+++ src/main/kotlin/Instructions/AndOr.kt
@@ -2,6 +2,6 @@ package compiler.Instructions
 
 class AndOr(private val type: String, private val dstReg: Register, private val src1: Register, private val src2: Register) : Instruction {
     override fun toString(): String {
-        return ("$type $dstReg, $src1, $src2")
+        return ("\t$type $dstReg, $src1, $src2")
     }
 }
