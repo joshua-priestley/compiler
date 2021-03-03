edb0843bf5b78a383a40930ba84bb6902557e02f
RobJBarr <robjb12@gmail.com>
2021-03-03 00:35:25 +0000

    Changed load to allow for LDRSB instruction

--- src/main/kotlin/Instructions/Load.kt
+++ src/main/kotlin/Instructions/Load.kt
@@ -8,34 +8,39 @@ class Load : Instruction {
     private val srcValue: String?
     private val offset: Int
     private val cond: Conditions?
+    private val sb: Boolean
 
-    constructor(dstReg: Register, srcValue: String, condition: Conditions? = null) {
+    constructor(dstReg: Register, srcValue: String, condition: Conditions? = null, sb : Boolean = false) {
         this.dstReg = dstReg
         this.srcValue = srcValue
         this.srcReg = null
         this.offset = 0
         this.cond = condition
+        this.sb = sb
     }
 
-    constructor(dstReg: Register, srcReg: Register, condition: Conditions? = null) {
+    constructor(dstReg: Register, srcReg: Register, condition: Conditions? = null, sb : Boolean = false) {
         this.dstReg = dstReg
         this.srcReg = srcReg
         this.srcValue = null
         this.offset = 0
         this.cond = condition
+        this.sb = sb
     }
 
-    constructor(dstReg: Register, srcReg: Register, offset: Int, condition: Conditions? = null) {
+    constructor(dstReg: Register, srcReg: Register, offset: Int, condition: Conditions? = null, sb : Boolean = false) {
         this.dstReg = dstReg
         this.srcReg = srcReg
         this.srcValue = null
         this.offset = offset
         this.cond = condition
+        this.sb = sb
     }
 
     override fun toString(): String {
         val instr = StringBuilder()
         instr.append("\tLDR")
+        if (sb) instr.append("SB")
         if (cond != null) instr.append(cond)
         instr.append(" $dstReg, ")
         if (srcReg != null) {
