a4883b3fcd3ad07d86294402e5ef5b3d2f134eb8
Euan Imperial <es1519@ic.ac.uk>
2021-03-03 22:13:48 +0000

    Added the printing of references.

--- src/main/kotlin/Instructions/Predefined.kt
+++ src/main/kotlin/Instructions/Predefined.kt
@@ -28,17 +28,19 @@ class PredefinedFuncs(private val data: DataSegment) {
 
     // Get the list of all instructions for external functions
     fun toInstructionList(): List<Instruction> =
-        funcSet.toList()
-            .map { it.getInstructions(data) }
-            .flatten()
+            funcSet.toList()
+                    .map { it.getInstructions(data) }
+                    .flatten()
 }
 
-abstract class Predefined() {
-   abstract val name: String
+//TODO finish once arithmetic ops have been done
+abstract class Predefined {
+    // Superclasses should use "by lazy" to get the msg label after it is added
+    abstract val name: String
     abstract val msg: String
     open val msg2: String? = null
 
-    abstract fun getInstructions(data: DataSegment) : List<Instruction>
+    abstract fun getInstructions(data: DataSegment): List<Instruction>
 
     override fun hashCode(): Int {
         return name.hashCode()
@@ -53,228 +55,231 @@ abstract class Predefined() {
     }
 }
 
-class PrintLn() : Predefined() {
+abstract class RuntimeError : Predefined()
+
+
+class PrintLn : Predefined() {
     override val name = "p_print_ln"
     override val msg = "\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("puts", true),
-            Move(Register.r0, ImmOp(0)),
-            Branch("fflush", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("puts", true),
+                    Move(Register.r0, ImmOp(0)),
+                    Branch("fflush", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class PrintString() : Predefined() {
+class PrintString : Predefined() {
     override val name = "p_print_string"
     override val msg = "%.*s\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Load(Register.r1, Register.r0),
-            Add(Register.r2, Register.r0, ImmOp(4), false),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("printf", true),
-            Move(Register.r0, ImmOp(0)),
-            Branch("fflush", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Load(Register.r1, Register.r0),
+                    Add(Register.r2, Register.r0, ImmOp(4), false),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("printf", true),
+                    Move(Register.r0, ImmOp(0)),
+                    Branch("fflush", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class PrintInt() : Predefined() {
+class PrintInt : Predefined() {
     override val name = "p_print_int"
     override val msg = "%d\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Move(Register.r1, Register.r0),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("printf", true),
-            Move(Register.r0, ImmOp(0)),
-            Branch("fflush", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Move(Register.r1, Register.r0),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("printf", true),
+                    Move(Register.r0, ImmOp(0)),
+                    Branch("fflush", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
 
-class PrintBool() : Predefined() {
+class PrintBool : Predefined() {
     override val name = "p_print_bool"
     override val msg = "true\\0"
     override val msg2 = "false\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Compare(Register.r0, ImmOp(0)),
-            Load(Register.r0, data.getLabel(msg), Conditions.NE),
-            Load(Register.r0, data.getLabel(msg2), Conditions.EQ),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("printf", true),
-            Move(Register.r0, ImmOp(0)),
-            Branch("fflush", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Compare(Register.r0, ImmOp(0)),
+                    Load(Register.r0, data.getLabel(msg), Conditions.NE),
+                    Load(Register.r0, data.getLabel(msg2), Conditions.EQ),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("printf", true),
+                    Move(Register.r0, ImmOp(0)),
+                    Branch("fflush", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class PrintReference() : Predefined() {
+class PrintReference : Predefined() {
     override val name = "p_print_reference"
     override val msg = "%p\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Move(Register.r1, Register.r0),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4)),
-            Branch("printf", true),
-            Move(Register.r0, ImmOp(0)),
-            Branch("fflush", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Move(Register.r1, Register.r0),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4)),
+                    Branch("printf", true),
+                    Move(Register.r0, ImmOp(0)),
+                    Branch("fflush", true),
+                    Pop(listOf(Register.pc))
+            )
+
+
 }
 
-class ReadInt() : Predefined() {
+class ReadInt : Predefined() {
     override val name = "p_read_int"
     override val msg = "%d\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Move(Register.r1, Register.r0),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("scanf", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Move(Register.r1, Register.r0),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("scanf", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
 // TODO potentially combine with ReadInt to avoid duplication?
-class ReadChar() : Predefined() {
+class ReadChar : Predefined() {
     override val name = "p_read_char"
     override val msg = "%c\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Move(Register.r1, Register.r0),
-            Load(Register.r0, data.getLabel(msg)),
-            Add(Register.r0, Register.r0, ImmOp(4), false),
-            Branch("scanf", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Move(Register.r1, Register.r0),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Add(Register.r0, Register.r0, ImmOp(4), false),
+                    Branch("scanf", true),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class ThrowRuntimeError() : Predefined() {
+class ThrowRuntimeError : Predefined() {
     override val name = "p_throw_runtime_error"
     override val msg = ""
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Branch(PrintString().name, true),
-            Move(Register.r0, ImmOp(-1)),
-            Branch("exit", true)
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Branch(PrintString().name, true),
+                    Move(Register.r0, ImmOp(-1)),
+                    Branch("exit", true)
+            )
 }
 
-abstract class RuntimeError() : Predefined()
-
-class DivideByZero() : RuntimeError() {
+class DivideByZero : RuntimeError() {
     override val name = "p_check_divide_by_zero"
     override val msg = "DivideByZeroError: divide or modulo by zero\\n\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Compare(Register.r1, ImmOp(1)),
-            Load(Register.r0, data.getLabel(msg), Conditions.EQ),
-            Branch(ThrowRuntimeError().name, true, Conditions.EQ),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Compare(Register.r1, ImmOp(1)),
+                    Load(Register.r0, data.getLabel(msg), Conditions.EQ),
+                    Branch(ThrowRuntimeError().name, true, Conditions.EQ),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class Overflow() : RuntimeError() {
+class Overflow : RuntimeError() {
     override val name = "p_throw_overflow_error"
     override val msg = "OverflowError: the result is too small/large to store in a 4-byte signed-integer.\\n\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Load(Register.r0, data.getLabel(msg)),
-            Branch(ThrowRuntimeError().name, true, Conditions.EQ)
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Branch(ThrowRuntimeError().name, true, Conditions.EQ)
+            )
 }
 
-class CheckNullPointer() : RuntimeError() {
+class CheckNullPointer : RuntimeError() {
     override val name = "p_check_null_pointer"
     override val msg = "NullReferenceError: dereference a null reference.\\n\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Compare(Register.r0, ImmOp(0)),
-            Load(Register.r0, data.getLabel(msg)),
-            Branch(ThrowRuntimeError().name, true, Conditions.EQ),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Compare(Register.r0, ImmOp(0)),
+                    Load(Register.r0, data.getLabel(msg)),
+                    Branch(ThrowRuntimeError().name, true, Conditions.EQ),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class CheckArrayBounds() : RuntimeError() {
+class CheckArrayBounds : RuntimeError() {
     override val name = "p_check_array_bounds"
     override val msg = "ArrayIndexOutOfBoundsError: negative index\\n\\0"
     override val msg2 = "ArrayIndexOutOfBoundsError: index too large\\n\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Compare(Register.r0, ImmOp(0)),
-            Load(Register.r0, data.getLabel(msg), Conditions.LT),
-            Branch(ThrowRuntimeError().name, true, Conditions.LT),
-            Load(Register.r1, Register.r1),
-            Compare(Register.r0, Register.r1),
-            Load(Register.r0, data.getLabel(msg2), Conditions.CS),
-            Branch(ThrowRuntimeError().name, true, Conditions.CS),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Compare(Register.r0, ImmOp(0)),
+                    Load(Register.r0, data.getLabel(msg), Conditions.LT),
+                    Branch(ThrowRuntimeError().name, true, Conditions.LT),
+                    Load(Register.r1, Register.r1),
+                    Compare(Register.r0, Register.r1),
+                    Load(Register.r0, data.getLabel(msg2), Conditions.CS),
+                    Branch(ThrowRuntimeError().name, true, Conditions.CS),
+                    Pop(listOf(Register.pc))
+            )
 }
 
-class Freepair() : RuntimeError() {
+class Freepair : RuntimeError() {
     override val name = "p_free_pair"
     override val msg = "NullReferenceError: dereference a null reference\\n\\0"
 
     override fun getInstructions(data: DataSegment): List<Instruction> =
-        listOf(
-            FunctionDeclaration(name),
-            Push(listOf(Register.lr)),
-            Compare(Register.r0, ImmOp(0)),
-            Load(Register.r0, data.getLabel(msg), Conditions.EQ),
-            Branch(ThrowRuntimeError().name, false, Conditions.EQ),
-            Push(listOf(Register.r0)),
-            Load(Register.r0, Register.r0),
-            Branch("free", true),
-            Load(Register.r0, Register.sp),
-            Load(Register.r0, Register.r0, 4),
-            Branch("free", true),
-            Pop(listOf(Register.r0)),
-            Branch("free", true),
-            Pop(listOf(Register.pc))
-        )
+            listOf(
+                    FunctionDeclaration(name),
+                    Push(listOf(Register.lr)),
+                    Compare(Register.r0, ImmOp(0)),
+                    Load(Register.r0, data.getLabel(msg), Conditions.EQ),
+                    Branch(ThrowRuntimeError().name, false, Conditions.EQ),
+                    Push(listOf(Register.r0)),
+                    Load(Register.r0, Register.r0),
+                    Branch("free", true),
+                    Load(Register.r0, Register.sp),
+                    Load(Register.r0, Register.r0, 4),
+                    Branch("free", true),
+                    Pop(listOf(Register.r0)),
+                    Branch("free", true),
+                    Pop(listOf(Register.pc))
+            )
 }
