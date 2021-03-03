20c77184b383254ed4419e94e63a7f20a6434eae
RobJBarr <robjb12@gmail.com>
2021-03-03 19:14:48 +0000

    Added pushing to stack when there are no more free registers

--- src/main/kotlin/Instructions/CodeGeneration.kt
+++ src/main/kotlin/Instructions/CodeGeneration.kt
@@ -517,80 +517,95 @@ class CodeGeneration(private var globalSymbolTable: SymbolTable) {
 
     private fun generateBinOp(binOp: BinaryOpNode, reg: Register = Register.r4): List<Instruction> {
         val list = mutableListOf<Instruction>()
-        val operand2 = reg.nextAvailable()
-        val expr1 = generateExpr(binOp.expr1, reg)
+        var operand1 = reg
+        var pop = false
+        if (operand1 >= Register.r10){
+            pop = true
+            operand1 = Register.r10
+            list.addAll(generateExpr(binOp.expr1,operand1))
+            list.add(Push(mutableListOf(Register.r10)))
+        } else {
+            list.addAll(generateExpr(binOp.expr1,operand1))
+        }
+        var operand2 = operand1.nextAvailable()
+        if (operand2 >= Register.r10) operand2 = Register.r10
         val expr2 = generateExpr(binOp.expr2, operand2)
-        list.addAll(expr1)
         list.addAll(expr2)
+
+        var dstRegister = operand1
+        if (pop){
+            list.add(Pop(mutableListOf(Register.r11)))
+            dstRegister = operand2
+            operand1 = Register.r11
+        }
 
         when (binOp.operator) {
             BinOp.PLUS -> {
-
-                list.add(Add(reg, reg, operand2, true))
+                list.add(Add(dstRegister, operand1, operand2, true))
                 list.add(Branch(predefined.addFunc(Overflow()), true, Conditions.VS))
             }
             BinOp.MINUS -> {
-                list.add(Sub(reg, reg, operand2, true))
+                list.add(Sub(dstRegister, operand1, operand2, true))
                 list.add(Branch(predefined.addFunc(Overflow()), true, Conditions.VS))
             }
             BinOp.MUL -> {
-                list.add(Multiply(reg, operand2, reg, operand2, true))
-                list.add(Compare(operand2, reg, null, ArithmeticShiftRight(31)))
+                list.add(Multiply(operand1, operand2, operand1, operand2, true))
+                list.add(Compare(operand2, operand1, null, ArithmeticShiftRight(31)))
                 list.add(Branch(predefined.addFunc(Overflow()), true, Conditions.NE))
             }
 
             BinOp.AND -> {
-                list.add(And(reg, reg, operand2))
+                list.add(And(dstRegister, operand1, operand2))
             }
             BinOp.OR -> {
-                list.add(Or(reg, reg, operand2))
+                list.add(Or(dstRegister, operand1, operand2))
             }
             BinOp.EQ -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.EQ))
-                list.add(Move(reg, 0, Conditions.NE))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.EQ))
+                list.add(Move(dstRegister, 0, Conditions.NE))
             }
             BinOp.NEQ -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.NE))
-                list.add(Move(reg, 0, Conditions.EQ))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.NE))
+                list.add(Move(dstRegister, 0, Conditions.EQ))
             }
             BinOp.LT -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.LT))
-                list.add(Move(reg, 0, Conditions.GE))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.LT))
+                list.add(Move(dstRegister, 0, Conditions.GE))
             }
             BinOp.GT -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.GT))
-                list.add(Move(reg, 0, Conditions.LE))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.GT))
+                list.add(Move(dstRegister, 0, Conditions.LE))
             }
             BinOp.GTE -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.GE))
-                list.add(Move(reg, 0, Conditions.LT))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.GE))
+                list.add(Move(dstRegister, 0, Conditions.LT))
             }
             BinOp.LTE -> {
-                list.add(Compare(reg, operand2))
-                list.add(Move(reg, 1, Conditions.LE))
-                list.add(Move(reg, 0, Conditions.GT))
+                list.add(Compare(operand1, operand2))
+                list.add(Move(dstRegister, 1, Conditions.LE))
+                list.add(Move(dstRegister, 0, Conditions.GT))
             }
 
             BinOp.MOD -> {
-                list.add(Move(Register.r0, reg))
+                list.add(Move(Register.r0, operand1))
                 list.add(Move(Register.r1, operand2))
                 val funcName = predefined.addFunc(DivideByZero())
                 list.add(Branch(funcName, true))
                 list.add(Branch("__aeabi_idivmod", true))
-                list.add(Move(reg, Register.r1))
+                list.add(Move(dstRegister, Register.r1))
             }
             BinOp.DIV -> {
-                list.add(Move(Register.r0, reg))
+                list.add(Move(Register.r0, operand1))
                 list.add(Move(Register.r1, operand2))
                 val funcName = predefined.addFunc(DivideByZero())
                 list.add(Branch(funcName, true))
                 list.add(Branch("__aeabi_idiv", true))
-                list.add(Move(reg, Register.r0))
+                list.add(Move(dstRegister, Register.r0))
             }
             else -> {
                 throw Error("Should not get here")
--- src/main/kotlin/Instructions/Register.kt
+++ src/main/kotlin/Instructions/Register.kt
@@ -1,6 +1,6 @@
 package compiler.Instructions
 
-const val NO_GEN_PURPOSE_REG = 11
+const val NO_GEN_PURPOSE_REG = 12
 
 enum class Register {
     r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, sp, lr, pc, cpsr;
