28d1eb5a296b324da7adad385a9960c7a80e9eb6
Euan Imperial <es1519@ic.ac.uk>
2021-03-03 20:09:46 +0000

    Added register shifts for accessing values and added extra classes that Operand2.kt can support.

--- src/main/kotlin/Instructions/Operand2.kt
+++ src/main/kotlin/Instructions/Operand2.kt
@@ -5,6 +5,15 @@ interface Operand2
 data class ImmOp(val imm: Int) : Operand2 {
     override fun toString(): String = "#$imm"
 }
+
 data class CharOp(val chr: Char) : Operand2 {
     override fun toString(): String = "#'$chr'"
+}
+
+data class ShiftInstruction(val reg: Register, val shiftType: ShiftType, val shift: Int) : Operand2 {
+    override fun toString(): String = "$reg, $shiftType, #$shift"
+}
+
+data class RegOp(val reg: Register): Operand2 {
+    override fun toString(): String = reg.toString()
 }
