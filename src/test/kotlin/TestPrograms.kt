26b5807eb846105ea9a3370a801b5022093dff01
Joshua Priestley <joshuapriestley@Joshuas-MacBook-Pro.local>
2021-03-04 15:15:26 +0000

    Trying to split commands up for ProcessBuilder to run

--- src/test/kotlin/TestPrograms.kt
+++ src/test/kotlin/TestPrograms.kt
@@ -74,8 +74,7 @@ class TestPrograms {
         println(stdin)
 
         // Run QEMU on the created executable file
-        val qemu = ProcessBuilder("/bin/sh", "-c", "echo", stdin, "|", "qemu-arm", "-L", "/usr/arm-linux-gnueabi/",
-            executableName).start()
+        val qemu = ProcessBuilder("/bin/sh", "-c", "echo $stdin", "|", "qemu-arm -L /usr/arm-linux-gnueabi/ $executableName").start()
 
         // Read the content produced by qemu
         val outputContent = StringBuilder()
