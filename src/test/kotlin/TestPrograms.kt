374b4d552ab6fdbb20653d7b5c67e2c2ab398003
Joshua Priestley <joshuapriestley@Joshuas-MacBook-Pro.local>
2021-03-04 15:20:27 +0000

    Wrapped the command in single quotes

--- src/test/kotlin/TestPrograms.kt
+++ src/test/kotlin/TestPrograms.kt
@@ -74,7 +74,7 @@ class TestPrograms {
         println(stdin)
 
         // Run QEMU on the created executable file
-        val qemu = ProcessBuilder("/bin/sh", "-c", "echo $stdin", "|", "qemu-arm -L /usr/arm-linux-gnueabi/ $executableName").start()
+        val qemu = Runtime.getRuntime().exec("/bin/sh -c 'echo $stdin | qemu-arm -L /usr/arm-linux-gnueabi/ $executableName'")
 
         // Read the content produced by qemu
         val outputContent = StringBuilder()
