3d088c8735cb6c2549d4d826abf79986e3cfbc38
Euan Imperial <es1519@ic.ac.uk>
2021-03-05 11:31:56 +0000

    Added brackets, was an issue with addition in if statement

--- src/main/kotlin/Instructions/CodeGeneration.kt
+++ src/main/kotlin/Instructions/CodeGeneration.kt
@@ -603,7 +603,7 @@ class CodeGeneration(private var globalSymbolTable: SymbolTable) {
     private fun getStackOffsetValue(name: String): Int {
         //println("${globalSymbolTable.getStackOffset(name)}, ${globalSymbolTable.localStackSize()}, $stackToAdd")
         return if (globalSymbolTable.getNodeGlobal(name)!!.isParameter()) {
-            globalSymbolTable.getStackOffset(name) + if (globalSymbolTable.containsNodeLocal(name)) globalSymbolTable.localStackSize() else 0 + if (assign && !globalSymbolTable.containsNodeLocal(name)) stackToAdd else 0
+            globalSymbolTable.getStackOffset(name) + (if (globalSymbolTable.containsNodeLocal(name)) globalSymbolTable.localStackSize() else 0) + if (assign && !globalSymbolTable.containsNodeLocal(name)) stackToAdd else 0
         } else {
             println("Name: $name, Local Stack: ${globalSymbolTable.localStackSize()}, ${globalSymbolTable.getStackOffset(name)}, ${if (assign && !globalSymbolTable.containsNodeLocal(name)) stackToAdd else 0}")
             globalSymbolTable.localStackSize() - globalSymbolTable.getStackOffset(name) + if (assign && !globalSymbolTable.containsNodeLocal(name)) stackToAdd else 0
