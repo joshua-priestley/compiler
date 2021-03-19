dbed5c0b2d8f57d140328a2149a02d0a060bb4ca
RobJBarr <robjb12@gmail.com>
2021-03-19 18:08:37 +0000

    Added fix to functions

--- src/main/kotlin/CodeGen/CodeGeneration.kt
+++ src/main/kotlin/CodeGen/CodeGeneration.kt
@@ -330,7 +330,7 @@ class CodeGeneration(private var globalSymbolTable: SymbolTable) {
             call.argList.map { x -> getType(x)!! }
         }
         //val args = rhs.argList!!.map { x -> getExprType(x,ctx) }
-        val string = call.ident.name + args.joinToString(separator = "_")
+        val string = call.ident.name + args.joinToString(separator = "_").replace("[]","ARR")
         var functionName = if (classFunc) "c_" else ""
         functionName += "f_$string"
         callInstructions.add(Branch(functionName, true))
